278189ad1221a5e26de37bcc418a0bcb43f2f58d
Sabrina Maldaner <sabrinasp1982@hotmail.com>
2021-03-22 17:05:19 -0300

    começando a implementar

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, MenuController, NavController } from 'ionic-angular';
 import { CredenciaisDTO } from '../../models/credenciais.dto';
+import { AuthService } from '../../services/auth.service';
 
 @IonicPage()
 @Component({
@@ -14,9 +15,13 @@ export class HomePage {
     senha: ""
   };
 
-  constructor(public navCtrl: NavController, public menu: MenuController) {
+  constructor(
+    public navCtrl: NavController, 
+    public menu: MenuController,
+    public auth: AuthService) {
 
   }
+  
   ionViewWillEnter() {
     this.menu.swipeEnable(false);
   }
@@ -24,10 +29,12 @@ export class HomePage {
   ionViewDidLeave() {
     this.menu.swipeEnable(true);
   }
-
   login() {
-    console.log(this.creds),
-    this.navCtrl.setRoot('CategoriasPage');
-    }
-
+    this.auth.authenticate(this.creds)
+      .subscribe(response => {
+        console.log(response.headers.get('Authorization'));
+        this.navCtrl.setRoot('CategoriasPage');
+      },
+      error => {});    
+  }
 }
